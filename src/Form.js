a974684462c643139805f091407e650440b748b3
Anna Petelin <adpetelin@gmail.com>
2022-11-08 18:35:49 +0200

    complete except for popup view component css

--- src/Form.js
+++ src/Form.js
@@ -10,7 +10,7 @@ function Form(props) {
                 <input name="firstName" type="text" maxLength={50} id="firstName"></input><br />
                 <label htmlFor="lastName">Lastname</label>
                 <input name="lastName" type="text" maxLength={50} id="lastName"></input><br />
-                <label htmlFor="phonenumber">Phonenumber</label>
+                <label htmlFor="phonenumber">Phone number</label>
                 <input type="text" name="phonenumber" maxLength={14} id="phonenumber"></input><br />
                 <label htmlFor="message">Message</label>
                 <textarea name="message" id="message" maxLength={200}></textarea><br />
--- src/View.js
+++ src/View.js
@@ -6,7 +6,7 @@ function View(props) {
         <div className="view">
             <p><span className="proforma">First name</span><span className='handwriting'>{props.firstName}</span></p>
             <p><span className="proforma">Last name</span><span className='handwriting'>{props.lastName}</span></p>
-            <p><span className="proforma">Phonenumber</span><span className='handwriting'>{props.phonenumber}</span></p>
+            <p><span className="proforma">Phone Number</span><span className='handwriting'>{props.phonenumber}</span></p>
             <p><span className="proforma">Message</span><span className='handwriting'>{props.message}</span></p>
             <p><span className="proforma">Role</span><span className='handwriting'>{props.role}</span></p>
         </div>
